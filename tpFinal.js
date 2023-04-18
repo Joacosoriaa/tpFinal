2913aefe36afa9e6eac830310c05bb4afe4bbc16
Joaquin Soria <joacosoria.js@gmail.com>
2023-04-18 17:05:53 -0300

    Integracion de formularios (falta footer)

---
+++ tpFinal.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $("#contact-btn").click(function() {
+      $("#contact-modal").modal("show");
+    });
+  });
+
+  
+
+  $(document).ready(function() {
+    $("#cotizacion-btn").click(function() {
+      $("#cotizacion-modal").modal("show");
+    });
+  });
